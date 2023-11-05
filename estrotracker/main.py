aed7628ec113eb95b807d861bb4e0ee89eb050a5
Sam Johnson <sambjohnson94@gmail.com>
2023-11-04 20:35:00 -0700

    ripped out tailwind, add bem cards

--- estrotracker/main.py
+++ estrotracker/main.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['STATIC_FOLDER'] = 'static'
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/deck")
+def deck():
+    return render_template("deck.html")
 
 
 if __name__ == "__main__":
